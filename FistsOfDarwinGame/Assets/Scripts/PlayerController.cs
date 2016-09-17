13f855f0a4612f06c3391c99c99425d64bf6a54e
Lucas <l.j.b.fejes@gmail.com>
2016-09-16 22:55:13 -0400

    Still working on my player stuff

--- FistsOfDarwinGame/Assets/Scripts/PlayerController.cs
+++ FistsOfDarwinGame/Assets/Scripts/PlayerController.cs
@@ -12,7 +12,8 @@ public class PlayerController : MonoBehaviour {
 
     Transform guiSpeedElement = null;
 
-    private float normalizeSpeed    = 0.5f;
+	public float velocity			= 0;
+    private float normalizeSpeed    = 2.0f;
     private Vector3 force           = Vector3.zero;
 
     bool horizontalOrientation      = true;
@@ -26,22 +27,29 @@ public class PlayerController : MonoBehaviour {
             Screen.orientation = ScreenOrientation.Portrait;
         }
         guiSpeedElement.position = new Vector3(0, normalizeSpeed, 0);
+
 	}                                                                                                                                                                                                                                                                                                                                                                                                                    
 	
-	// Update is called once per frame
-	void Update () {
-	    foreach (Touch evt in Input.touches) {
-            if (evt.phase == TouchPhase.Moved) {
-                normalizeSpeed = evt.position.y / Screen.height;
-                guiSpeedElement.position = new Vector3 (0, normalizeSpeed, 0);
-            }
-        }
-	}
+	//// Update is called once per frame
+	//void Update () {
+	//    foreach (Touch evt in Input.touches) {
+ //           if (evt.phase == TouchPhase.Moved) {
+ //               normalizeSpeed = evt.position.y / Screen.height;
+ //               guiSpeedElement.position = new Vector3 (0, normalizeSpeed, 0);
+ //           }
+ //       }
+	//}
 
     void FixedUpdate() {
-        normalizeSpeed = 2.0f;
+		velocity = normalizeSpeed * forwardForce;
 
-        GetComponent<Rigidbody>().AddForce(0, 0, normalizeSpeed * forwardForce);
+		if ( velocity >= 2.0f) {
+			GetComponent<Rigidbody>().AddForce(0, 0, velocity);
+		}
+		else
+		{
+			GetComponent<Rigidbody>().AddForce(0, 0, 0);
+		}
 
 		Vector3 accelerator = Input.acceleration;        
 
@@ -49,7 +57,8 @@ public class PlayerController : MonoBehaviour {
         force.z = Mathf.Lerp(force.z, -accelerator.x * maxTurnLean, 0.2f);
         force.x = Mathf.Lerp(force.x, (accelerator.y + 0.6f - 0.6f) * maxTilt, 0.2f);
 
-        Quaternion rot = Quaternion.Euler(force);
-        transform.rotation = Quaternion.Lerp(transform.rotation, rot, sensitivity);
+		// Rotates the Camera
+        //Quaternion rot = Quaternion.Euler(force);
+        //transform.rotation = Quaternion.Lerp(transform.rotation, rot, sensitivity);
     }
 } 
