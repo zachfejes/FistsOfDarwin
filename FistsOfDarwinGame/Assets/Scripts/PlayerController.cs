09df1f28de4f76d1b6c1b6d9dfeb4ef3d790bb64
Lucas <l.j.b.fejes@gmail.com>
2016-09-17 19:13:32 -0400

    Update
    Got movement working, as well as camera

--- FistsOfDarwinGame/Assets/Scripts/PlayerController.cs
+++ FistsOfDarwinGame/Assets/Scripts/PlayerController.cs
@@ -3,38 +3,41 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
-    float turnSpeed     = 10.0f;
+    float turnSpeed     = 5.0f;
     float maxTurnLean	= 50.0f;
     float maxTilt       = 50.0f;
 
     float sensitivity   = 10.0f;
-    float forwardForce  = 1.2f;
+    float forwardForce  = 3.2f;
 
     public Transform guiSpeedElement;
 
 	public float maxSpeed			= 5.0f;
 
 	public float velocity			= 0.0f;
+	public float movement			= 0.0f;
     private float normalizeSpeed    = 2.0f;
     private Vector3 force           = Vector3.zero;
 
-    bool horizontalOrientation      = true;
+	bool horizontalOrientation      = true;
 
 	// Use this for initialization
-	void Start () {
-        if (horizontalOrientation) {
-            Screen.orientation = ScreenOrientation.LandscapeLeft;
-        }
-        else {
-            Screen.orientation = ScreenOrientation.Portrait;
-        }
-        guiSpeedElement.position = new Vector3(0, normalizeSpeed, 0);
+	void Start()
+	{
+		if (horizontalOrientation)
+		{
+			Screen.orientation = ScreenOrientation.LandscapeLeft;
 
+		}
+		else
+		{
+			Screen.orientation = ScreenOrientation.Portrait;
+		}
+		guiSpeedElement.position = new Vector3(0, normalizeSpeed, 0);
 	}
 
 	// Update is called once per frame
-	void Update()
-	{
+	void Update() {
 		foreach (Touch evt in Input.touches)
 		{
 			if (evt.phase == TouchPhase.Moved)
@@ -46,13 +49,8 @@ public class PlayerController : MonoBehaviour {
 	}
 
 	void FixedUpdate() {
-
+		Vector3 accelerator = Input.acceleration;
 		velocity = Vector3.Magnitude(GetComponent<Rigidbody>().velocity);
-
-		// Sets max speed
-		if (velocity <= maxSpeed) {
-			GetComponent<Rigidbody>().AddForce(0, 0, normalizeSpeed * forwardForce);
-		}
 
 		// Steering accelerations
 		var directionalInput = new Vector3(
@@ -61,11 +59,16 @@ public class PlayerController : MonoBehaviour {
 			Input.acceleration.y
 		) * velocity;
 
-		Vector3 accelerator = Input.acceleration;        
-
-        force.y += accelerator.x * turnSpeed;
+		// Rotates the Player
+		force.y += accelerator.x * turnSpeed;
         force.z = Mathf.Lerp(force.z, -accelerator.x * maxTurnLean, 0.2f);
         force.x = Mathf.Lerp(force.x, (accelerator.y + 0.6f - 0.6f) * maxTilt, 0.2f);
+
+		// Sets max speed
+		if (velocity <= maxSpeed) {
+			GetComponent<Rigidbody>().AddForce(transform.forward * forwardForce);
+
+		}
 
 		// Rotates the Camera
         Quaternion rot = Quaternion.Euler(force);
