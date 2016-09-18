ba188ffdfce681e5752ec78995f683392d6e1c5b
wilcroft <wilcroft@gmail.com>
2016-09-18 15:52:31 -0400

    adding avatar swapping maybe?

--- FistsOfDarwinGame/Assets/Scripts/playerState.cs
+++ FistsOfDarwinGame/Assets/Scripts/playerState.cs
@@ -5,6 +5,10 @@ public class playerState : MonoBehaviour {
 
 	public timeScaleUpdate worldState;
 
+	public GameObject avatar;
+
+	public creatureManager creatures;
+
 	timePeriod.timePeriodEnum playerEra;
 	// Use this for initialization
 	void Start () {
@@ -14,9 +18,34 @@ public class playerState : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
 	{
-		if (worldState.playerEra != playerEra)
+		if (worldState.playerEra != playerEra) {
 			Debug.Log("Level Up!");
+			Vector3 loc = avatar.transform.position;
+			PlayerController pc = avatar.GetComponent<PlayerController>();
+
+			//swap out avatar
+			if (!worldState.gameIsActive())
+				;
+			//somehow remove avatar
+			if (playerEra == timePeriod.timePeriodEnum.cambrianStage2) {
+				avatar = Instantiate(creatures.amoebaPrefab);
+				avatar.transform.position = loc;
+				avatar.AddComponent<PlayerController>();
+			}
+			if (playerEra == timePeriod.timePeriodEnum.cambrianStage4) {
+				avatar = Instantiate(creatures.wormPrefab);
+				avatar.transform.position = loc;
+				avatar.AddComponent<PlayerController>();
+			}
+			if (playerEra == timePeriod.timePeriodEnum.cambrianStage5) {
+				avatar = Instantiate(creatures.trilobitePrefab);
+				avatar.transform.position = loc;
+				avatar.AddComponent<PlayerController>();
+			}
+		}
 		playerEra = worldState.playerEra;
+
+			
 
 	}
 }
